7738ccf82606005109005312b9091419d5cb3a0f
TayouzaDev <tayouzadev@gmail.com>
2022-12-03 00:39:09 -0300

    add: makefile

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,10 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastname',
         'email',
         'password',
-        'name',
-        'lastname',
         'situation'
     ];
 
