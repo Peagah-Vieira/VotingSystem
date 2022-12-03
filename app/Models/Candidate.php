0a30e39c0dca70dc1300da92c5c8b809b855f313
TayouzaDev <tayouzadev@gmail.com>
2022-12-03 00:04:46 -0300

    mudanças na nomeclatura

---
+++ app/Models/Candidate.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Candidate extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'lastname',
+        'state',
+        'party_affiliate',
+        'number',
+        'situation'
+    ];
+}
--- database/factories/CandidateFactory.php
+++ database/factories/CandidateFactory.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Candidate>
  */
-class UserFactory extends Factory
+class CandidateFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -20,10 +20,6 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'lastname' => fake()->lastname(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
         ];
     }
 
