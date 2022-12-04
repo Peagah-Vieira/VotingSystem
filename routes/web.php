b070ac90ae722556a850fa3a75e1bd2443033148
Peagah <peagahvieira2003@gmail.com>
2022-12-03 21:12:39 -0300

    add: design for login,register and recover

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/login', 'login')->name('login');
+
+Route::view('/register', 'register')->name('register');
+
+Route::view('/forgotPassword', 'forgotPassword')->name('forgotPassword');
