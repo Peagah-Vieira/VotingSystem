ea7e76ab44daf69ac575d4ab87b0715981fcb271
Peagah <peagahvieira2003@gmail.com>
2022-12-04 00:40:19 -0300

    add: adjusted submit

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 use App\Http\Auth\UserAuth;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
-    
+Route::get('/', function() {
+    return view('login');
 });
 
-Route::get('/', [UserAuth::class, 'index'])->name('login');
-Route::get('/register', [UserAuth::class, 'registerPage'])->name('register');
-Route::get('/forgotPassword', [UserAuth::class, 'forgotPassword'])->name('forgotPassword');
+Route::view('/login', 'login')->name('login');
+Route::view('/register', 'register')->name('register');
+Route::view('/forgotPassword', 'forgotPassword')->name('forgotPassword');
+Route::view('/home', 'home')->name('home');
 
-Route::post('/register', [UserAuth::class, 'register'])->name('auth.register');
-Route::post('/login', [UserAuth::class, 'login'])->name('auth.login');
+Route::post('/userRegister', [UserAuth::class, 'register'])->name('auth.register');
+Route::post('/loginSubmit', [UserAuth::class, 'login'])->name('auth.login');
